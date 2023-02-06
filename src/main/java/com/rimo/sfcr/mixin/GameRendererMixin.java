af4c8fc9c36927a2d0459f299ab2c6790f6262c7
Rivmun <113169310+Rivmun@users.noreply.github.com>
2023-02-07 03:56:30 +0800

    1.4.0 b1
    优化天气检测算法，开放更多的可配置项目，尝试使用降水值进行群系检测。
    问题：
    - 使用了 integrated server 类，可能无法在多人游戏中使用；
    - 降雨时的云层密度依旧无法令人满意；
    - 群系检测不起作用；
    - 云层常规密度设置不起作用；
    - 刷新频率在天气稳定后无法降低；
    - 雾设置在大改后与原生地面迷雾脱节（需还原更改）。
    （反正没正式发布并合并分支，就不用英文写日志了）

---
+++ src/main/java/com/rimo/sfcr/mixin/GameRendererMixin.java
@@ -0,0 +1,18 @@
+package com.rimo.sfcr.mixin;
+
+import com.rimo.sfcr.SFCReMod;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+	
+	//Prevent cloud be culled
+	@Inject(method = "method_32796", at = @At("RETURN"), cancellable = true)
+	private void extend_distance(CallbackInfoReturnable<Float> cir) {
+		cir.setReturnValue(cir.getReturnValue() * (SFCReMod.RENDERER.getFogDistance() + 2));
+	}
+}

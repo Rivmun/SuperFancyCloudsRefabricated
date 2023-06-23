d7fb67823be8607c4160d40950fac2a20aedee44
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-06-23 11:51:57 +0800

    1.19.4 rearchitected (#64)

---
+++ common/src/main/java/com/rimo/sfcr/mixin/GameOptionsMixin.java
@@ -0,0 +1,29 @@
+package com.rimo.sfcr.mixin;
+
+import com.rimo.sfcr.SFCReMod;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+
+@Mixin(GameOptions.class)
+public abstract class GameOptionsMixin {
+
+	@Shadow
+	private SimpleOption<Integer> viewDistance;
+	
+	//Update cloudRenderDistance when view distance is changed.
+	@Inject(method = "write", at = @At("RETURN"), cancellable = true)
+	private void updateCloudRenderDistance(CallbackInfo ci) {
+		if (SFCReMod.COMMON_CONFIG.isCloudRenderDistanceFitToView()) {
+			SFCReMod.COMMON_CONFIG.setCloudRenderDistance(viewDistance.getValue() * 12);
+			SFCReMod.COMMON_CONFIG_HOLDER.save();
+		}
+		ci.cancel();
+	}
+}
--- src/main/java/com/rimo/sfcr/config/CloudRefreshSpeed.java
+++
@@ -1,9 +0,0 @@
-package com.rimo.sfcr.config;
-
-public enum CloudRefreshSpeed {
-	VERY_SLOW,
-	SLOW,
-	NORMAL,
-	FAST,
-	VERY_FAST
-}

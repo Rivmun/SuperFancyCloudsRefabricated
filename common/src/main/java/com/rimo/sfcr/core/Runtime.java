335fd373886f5be1ba97604def18d56b8c09efed
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-13 18:33:22 +0800

    Add files via upload

--- common/src/main/java/com/rimo/sfcr/core/Runtime.java
+++ common/src/main/java/com/rimo/sfcr/core/Runtime.java
@@ -1,15 +1,20 @@
 package com.rimo.sfcr.core;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.rimo.sfcr.SFCReMod;
 import com.rimo.sfcr.config.CommonConfig;
 import com.rimo.sfcr.mixin.ServerWorldAccessor;
+import com.rimo.sfcr.network.ConfigSyncMessage;
 import com.rimo.sfcr.network.Network;
+import com.rimo.sfcr.network.RuntimeSyncMessage;
 import com.rimo.sfcr.util.WeatherType;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.World;
@@ -25,19 +30,25 @@ public class Runtime {
 
 	private RegistryKey<World> worldKey;
 	public WeatherType nextWeather = WeatherType.CLEAR;
-
-	private double lastSyncTime = 0;
+	private List<ServerPlayerManager> playerList;
 
 	public void init(ServerWorld world) {
 		seed = new Random().nextLong();
 		worldKey = world.getRegistryKey();
+		playerList = new ArrayList<>();
 	}
 
 	public void tick(MinecraftServer server) {
 
-		if (server.isDedicated())
-			partialOffset += 1 / 20f * 0.25f * 0.25f * CONFIG.getCloudBlockSize() / 16f;		// 20 tick per second.
-		time += 1 / 20f;
+		if (!CONFIG.isEnableMod())
+			return;
+
+		if (server.isDedicated()) {		// These data is updated by RENDERER, but dedicated server is not have, so we update here.
+			partialOffset += 1 / 20f * CONFIG.getCloudBlockSize() / 16f;
+			checkFullOffset();
+			checkPartialOffset();
+		}
+		time += 1 / 20f;		// 20 tick per second.
 
 		// Weather Pre-detect
 		var worldProperties = ((ServerWorldAccessor) server.getWorld(worldKey)).getWorldProperties();
@@ -62,7 +73,17 @@ public class Runtime {
 		if (nextWeather != currentWeather)
 			Network.sendWeather(server);
 
-		if (CONFIG.isEnableDebug() && server.getTicks() % (CONFIG.getWeatherPreDetectTime() * 20) == 0) {
+		// Data Sync
+		if (server.getTicks() % 20 == 0 && (server.isDedicated() || CONFIG.isEnableServerConfig())) {
+			playerList.forEach(manager -> {
+				if (manager.lastSyncTime < time - CONFIG.getSecPerSync()) {
+					Network.RUNTIME_CHANNEL.sendToPlayer(manager.player, new RuntimeSyncMessage(SFCReMod.RUNTIME));
+					manager.lastSyncTime = time;
+				}
+			});
+		}
+
+		if (CONFIG.isEnableDebug() && !server.isDedicated() && server.getTicks() % (CONFIG.getWeatherPreDetectTime() * 20) == 0) {
 			SFCReMod.LOGGER.info("isThnd: " + worldProperties.isThundering() + ", isRain: " + worldProperties.isRaining());
 			SFCReMod.LOGGER.info("thndTime: " + worldProperties.getThunderTime() + ", rainTime: " + worldProperties.getRainTime() + ", clearTime: " + worldProperties.getClearWeatherTime());
 			SFCReMod.LOGGER.info("nextWeather: " + nextWeather.toString());
@@ -70,20 +91,9 @@ public class Runtime {
 	}
 
 	public void clientTick(World world) {
-
 		// Weather Detect (Client) - Only runs when connected to a server without sync
-		if (!MinecraftClient.getInstance().isIntegratedServerRunning() && lastSyncTime == 0)
+		if (!MinecraftClient.getInstance().isIntegratedServerRunning() && CONFIG.isEnableServerConfig())
 			nextWeather = world.isThundering() ? WeatherType.THUNDER : world.isRaining() ? WeatherType.RAIN : WeatherType.CLEAR;
-
-		// Auto Sync
-		if (lastSyncTime < time - CONFIG.getSecPerSync()) {
-			Network.sendSyncRequest(false);
-			lastSyncTime = time;
-		}
-	}
-
-	public void clientEnd() {
-		lastSyncTime = 0;
 	}
 
 	public void checkFullOffset() {
@@ -92,5 +102,53 @@ public class Runtime {
 
 	public void checkPartialOffset() {
 		partialOffset = partialOffset % CONFIG.getCloudBlockSize();
+	}
+
+	public List<ServerPlayerManager> getPlayerList() {
+		return playerList;
+	}
+
+	public void addPlayer(ServerPlayerEntity player) {
+		if (!playerList.stream().filter(manager -> manager.getPlayer() == player).toList().isEmpty())
+			return;
+		playerList.add(new ServerPlayerManager(player));
+	}
+
+	public void removePlayer(ServerPlayerEntity player) {
+		playerList.removeIf(manager -> manager.getPlayer() == player);
+	}
+
+	public void end(MinecraftServer server) {
+		playerList.clear();
+	}
+
+	public void clientEnd() {
+		//
+	}
+
+	public class ServerPlayerManager {
+		private final ServerPlayerEntity player;
+		private double lastSyncTime;
+
+		ServerPlayerManager(ServerPlayerEntity player) {
+			this.player = player;
+			this.lastSyncTime = time;
+			if (player.getServer().isDedicated() || CONFIG.isEnableServerConfig()) {
+				Network.CONFIG_CHANNEL.sendToPlayer(player, new ConfigSyncMessage(seed, SFCReMod.COMMON_CONFIG));
+				Network.RUNTIME_CHANNEL.sendToPlayer(player, new RuntimeSyncMessage(SFCReMod.RUNTIME));
+			}
+		}
+
+		public ServerPlayerEntity getPlayer() {
+			return player;
+		}
+
+		public void setLastSyncTime(double time) {
+			lastSyncTime = time;
+		}
+
+		public double getLastSyncTime() {
+			return lastSyncTime;
+		}
 	}
 }

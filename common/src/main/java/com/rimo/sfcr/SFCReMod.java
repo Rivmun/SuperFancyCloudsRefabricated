2e5811ce02843a30c0076fea40d376c2b765b170
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-14 17:14:52 +0800

    Update SFCReMod.java

--- common/src/main/java/com/rimo/sfcr/SFCReMod.java
+++ common/src/main/java/com/rimo/sfcr/SFCReMod.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 public class SFCReMod {
 
 	public static final String MOD_ID = "sfcr";
-	public static final Logger LOGGER = LoggerFactory.getLogger("sfcr");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final CommonConfig COMMON_CONFIG = new CommonConfig();
 

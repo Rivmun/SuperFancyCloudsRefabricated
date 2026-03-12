adb70f2f1dc95b6dfe9086073ec8945942dcef6c
Rivmun <qxsav@126.com>
2026-03-12 13:12:45 +0800

    - replace @Redirect to other annotation that are not easily to cause conflict. Fix #117 - update particle rain compat

--- common/src/main/java/com/rimo/sfcr/mixin/particlerain/ParticleSpawnerMixin.java
+++ common/src/main/java/com/rimo/sfcr/mixin/particlerain/ParticleSpawnerMixin.java
@@ -1,5 +1,7 @@
 package com.rimo.sfcr.mixin.particlerain;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.rimo.sfcr.Client;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -7,51 +9,48 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import pigcart.particlerain.WeatherParticleManager;
-import pigcart.particlerain.config.ConfigData;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import pigcart.particlerain.ParticleSpawner;
+import pigcart.particlerain.config.ParticleData;
 
 import java.util.Iterator;
 
 import static com.rimo.sfcr.Common.CONFIG;
 
-@Mixin(WeatherParticleManager.class)
-public abstract class WeatherParticleManagerMixin {
+@Mixin(ParticleSpawner.class)
+public abstract class ParticleSpawnerMixin {
 	@Unique private static double sfcr$x, sfcr$y, sfcr$z;
 	@Unique private static boolean sfcr$shouldCancel;
 
 	// rain
-	@Redirect(method = "tickSkyFX", at = @At(
+	@ModifyArgs(method = "tickSkyFX", at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;set(DDD)Lnet/minecraft/core/BlockPos$MutableBlockPos;"
 	))
-	private static BlockPos.MutableBlockPos sfcr$catchRenderPos(BlockPos.MutableBlockPos instance, double x, double y, double z) {
-		sfcr$x = x;
-		sfcr$y = y;
-		sfcr$z = z;
-		return instance.set(x, y, z);
+	private static void sfcr$catchRenderPos(Args args) {
+		sfcr$x = args.get(0);
+		sfcr$y = args.get(1);
+		sfcr$z = args.get(2);
 	}
-	@Redirect(method = "tickSkyFX", at = @At(
+	@WrapOperation(method = "tickSkyFX", at = @At(
 			value = "INVOKE",
 			target = "Ljava/util/Iterator;next()Ljava/lang/Object;"  //inject into for(Object o : ArrayList<o> list)
 	))
-	private static Object sfcr$catchSkyFXParticleData(Iterator<ConfigData.ParticleData> instance) {
-		ConfigData.ParticleData data = instance.next();
-		sfcr$shouldCancel = data.weather == ConfigData.Weather.DURING_WEATHER && ! data.precipitation.contains(Biome.Precipitation.NONE);
+	private static Object sfcr$catchSkyFXParticleData(Iterator<ParticleData> instance, Operation<Object> original) {
+		ParticleData data = (ParticleData) original.call(instance);
+		sfcr$shouldCancel = data.weather == ParticleData.Weather.DURING_WEATHER && ! data.precipitation.contains(Biome.Precipitation.NONE);
 		return data;
 	}
-	@Redirect(method = "tickSkyFX", at = @At(
+	@WrapOperation(method = "tickSkyFX", at = @At(
 			value = "INVOKE",
-			target = "Lpigcart/particlerain/config/ConfigData$SpawnPos;equals(Ljava/lang/Object;)Z"
+			target = "Lpigcart/particlerain/config/ParticleData$SpawnPos;equals(Ljava/lang/Object;)Z"
 	))
-	private static boolean sfcr$shouldCancelSkyFX(ConfigData.SpawnPos instance, Object o) {
+	private static boolean sfcr$shouldCancelSkyFX(ParticleData.SpawnPos instance, Object o, Operation<Boolean> original) {
 		if (sfcr$shouldCancel && CONFIG.isEnableParticleRainCompat() && Client.isNoCloudCovered(sfcr$x, sfcr$y, sfcr$z))
 			return false;
-		return instance.equals(o);
+		return original.call(instance, o);
 	}
 
 	// still splash? do it again!
@@ -61,13 +60,13 @@ public abstract class WeatherParticleManagerMixin {
 		sfcr$y = sourcePos.getY();
 		sfcr$z = sourcePos.getZ();
 	}
-	@Redirect(method = "tickBlockFX", at = @At(
+	@WrapOperation(method = "tickBlockFX", at = @At(
 			value = "INVOKE",
 			target = "Ljava/util/Iterator;next()Ljava/lang/Object;"
 	))
-	private static Object sfcr$catchBlockFXParticleData(Iterator<ConfigData.ParticleData> instance) {
-		ConfigData.ParticleData data = instance.next();
-		sfcr$shouldCancel = data.weather == ConfigData.Weather.DURING_WEATHER && ! data.precipitation.contains(Biome.Precipitation.NONE);
+	private static Object sfcr$catchBlockFXParticleData(Iterator<ParticleData> instance, Operation<Object> original) {
+		ParticleData data = (ParticleData) original.call(instance);
+		sfcr$shouldCancel = data.weather == ParticleData.Weather.DURING_WEATHER && ! data.precipitation.contains(Biome.Precipitation.NONE);
 		return data;
 	}
 	@ModifyVariable(method = "tickBlockFX", at = @At("STORE"), name = "direction")
